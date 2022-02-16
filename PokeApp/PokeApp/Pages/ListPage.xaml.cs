5dc94e2f288eb5c494b0455f6ea3d0fefada9071
Romain Allemand <romainallemand0408@gmail.com>
2022-02-16 16:04:19 +0100

    Modification HomePage Responsive

--- PokeApp/PokeApp/Pages/ListPage.xaml.cs
+++ PokeApp/PokeApp/Pages/ListPage.xaml.cs
@@ -2,38 +2,17 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
+using PokeApp.ViewModels;
 
 namespace PokeApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListPage : ContentPage
     {
-        private ObservableCollection<Pokemon> myList;
-
-        private ObservableCollection<Pokemon> MyList
-        {
-            get { return myList; }
-            set { myList = value; }
-        }
         public ListPage()
         {
             InitializeComponent();
-            BindingContext = this;
-
-            MyList = new ObservableCollection<Pokemon>();
-
-            for (int i = 1; i < 10; i++)
-            {
-                MyList.Add(new Pokemon() { Name = "piplup" + i.ToString(), Number = i.ToString(), Type1 = "water" + i.ToString(), Type2 = "fire" + i.ToString(), UrlImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png" });
-            }
-
-            pokemonsList.ItemsSource = MyList;
+            BindingContext = ListViewModel.Instance;
         }
-
-        //async void ShowDetails(object sender, EventArgs args)
-        //{
-        //    await Navigation.PushAsync(new DetailsPage());
-        //}
-
     }
 }
--- PokeApp/PokeApp/ViewModels/ListViewModel.cs
+++ PokeApp/PokeApp/ViewModels/ListViewModel.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 
 namespace PokeApp.ViewModels
 {
-    public class ListViewModel : BaseViewModel
+    internal class ListViewModel : BaseViewModel
     {
         private static ListViewModel _instance = new ListViewModel();
-        public ListViewModel Instance { get { return _instance; } }
+        public static ListViewModel Instance { get { return _instance; } }
 
+        public ObservableCollection<Pokemon> MyList
+        {
+            get { return GetValue<ObservableCollection<Pokemon>>(); }
+            set { SetValue(value); }
+        }
+
+        public ListViewModel()
+        {
+            MyList = new ObservableCollection<Pokemon>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                MyList.Add(new Pokemon() { Name = "piplup" + i.ToString(), Number = i.ToString(), Type1 = "water" + i.ToString(), Type2 = "fire" + i.ToString(), UrlImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png" });
+            }
+        }
 
     }
 }
