aa5f62039b26c0df654f4202aa26cbf3a650f1cc
Romain Allemand <romainallemand0408@gmail.com>
2022-02-16 14:35:57 +0100

    Initial Commit et projet precedent avec gitignore

---
+++ PokeApp/PokeApp/Models/Pokemon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeApp
+{
+    internal class Pokemon
+    {
+        private String name;
+        public String Name { get { return name; } set { name = value; } }
+
+        private String description;
+        public String Description { get { return description; } set { description = value; } }
+
+        private String type1;
+        public String Type1 { get { return type1; } set { type1 = value; } }
+
+        private String type2;
+        public String Type2 { get { return type2; } set { type2 = value; } }
+
+        private String number;
+        public String Number { get { return "#" + number; } set { number = value; } }
+
+        private String ability1;
+        public String Ability1 { get { return ability1; } set { ability1 = value; } }
+       
+        private String ability2;
+        public String Ability2 { get { return ability2; } set { ability2 = value; } }
+
+        private String urlImg;
+        public String UrlImg { get { return urlImg; } set { urlImg = value; } }
+
+        private String evolution;
+        public String Evolution { get { return evolution; } set { evolution = value; } }
+    }
+}
---
+++ PokeApp/PokeApp/ViewModels/ListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeApp.ViewModels
+{
+    public class ListViewModel : BaseViewModel
+    {
+        private static ListViewModel _instance = new ListViewModel();
+        public ListViewModel Instance { get { return _instance; } }
+
+
+    }
+}
